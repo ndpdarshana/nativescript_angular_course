1e6b7604c7ddbd93c12008c855767c84c4dacba1
Prabhath Darshana <prabhath@huubap.com>
2020-05-11 23:55:36 +1000

    Test: StackLayout verticle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from "./app.component";
 import { CurrentChallengeComponent } from "./challenges/current-challenge/current-challenge.component";
+import { StackComponent } from './layouts/stack/stack.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -21,7 +22,8 @@ import { CurrentChallengeComponent } from "./challenges/current-challenge/curren
     ],
     declarations: [
         AppComponent,
-        CurrentChallengeComponent
+        CurrentChallengeComponent,
+        StackComponent
     ],
     providers: [],
     schemas: [
